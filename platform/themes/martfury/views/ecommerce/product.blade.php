65209ca48fff111b96495b64644c1fe6651ecd4c
nhphong0104 <nhphong0104@gmail.com>
2022-10-04 23:41:41 +0700

    up fix product

--- platform/themes/martfury/views/ecommerce/product.blade.php
+++ platform/themes/martfury/views/ecommerce/product.blade.php
@@ -201,7 +201,7 @@
                                         @if (EcommerceHelper::isReviewEnabled())
                                             <hr>
                                             <div id="tab-reviews">
-                                                <h3>{ __('Reviews') }}</h3>
+                                                <h3>{{ __('Reviews') }}</h3>
                                                 <div class="row">
                                                     <div class="col-lg-5">
                                                         <div class="ps-block--average-rating">
