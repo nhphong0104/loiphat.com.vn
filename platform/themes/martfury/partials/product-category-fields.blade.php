f6053269cff3435e111a3926664e21e2fc5b5c0c
nhphong0104 <nhphong0104@gmail.com>
2022-10-10 23:05:31 +0700

    up

--- platform/themes/martfury/partials/product-category-fields.blade.php
+++ platform/themes/martfury/partials/product-category-fields.blade.php
@@ -2,3 +2,7 @@
     <label class="control-label" for="icon">{{ __('Icon') }}</label>
     {!! Form::themeIcon('icon', $icon, ['class' =>'form-control', 'id' => 'icon-select']) !!}
 </div>
+<div class="form-group">
+    <label class="control-label" for="icon_image">{{ __('Icon Image (It will replace Font Icon if it is present)') }}</label>
+    {!! Form::mediaImage('icon_image', $iconImage) !!}
+</div>
