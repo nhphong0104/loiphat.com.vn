d0dcfe1535e97be7132e4cd9eb8644b11e2471e3
nhphong0104 <nhphong0104@gmail.com>
2024-07-04 22:45:28 +0700

    up

--- platform/themes/martfury/partials/header.blade.php
+++ platform/themes/martfury/partials/header.blade.php
@@ -25,6 +25,13 @@
         </style>
         <meta name='dmca-site-verification' content='aElFWDdYa0dxNThUb3FEL1NJRk0xdz090' />
         {!! Theme::header() !!}
+    <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NQTKF4Q');</script>
+        <!-- End Google Tag Manager -->
     </head>
     <body @if (Theme::get('pageId')) id="{{ Theme::get('pageId') }}" @endif @if (BaseHelper::siteLanguageDirection() == 'rtl') dir="rtl" @endif>
     <!-- Google Tag Manager (noscript) -->
